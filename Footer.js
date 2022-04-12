43f26c58ea034f25d89d4907524331bac5c01bfd
Stanley <stanley.wilder.24@gmail.com>
2022-04-11 19:31:13 -0700

    Lab completed

---
+++ Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+class Footer extends React.Component{
+  render(){
+    return(
+      <footer>
+        <p>
+          Stanley Wilder &amp;
+        </p>
+      </footer>
+    )
+  }
+}
+export default Footer;
