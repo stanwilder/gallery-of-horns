3ad9a939747243ac80acf949746f391fb75c1ee1
Stanley <stanley.wilder.24@gmail.com>
2022-04-11 19:46:03 -0700

    page was not rendering. switched things around.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
-
+import Header from './Header.js';
+import Main from './Main.js';
+import Footer from './Footer.js';
 class App extends React.Component {
   render() {
     return(
@@ -14,7 +16,7 @@ class App extends React.Component {
       
     
       </>
-  ); 
+  ) 
 }
 }
 
